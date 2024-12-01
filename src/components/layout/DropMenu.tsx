1e0cdbe50be75fe61d8def7b6cd0dbb09380c98d
AndDev <23031429@itcelaya.edu.mx>
2024-12-01 12:13:04 -0600

    Add dark mode support to DropdownMenu component

--- src/components/layout/DropMenu.tsx
+++ src/components/layout/DropMenu.tsx
@@ -13,19 +13,19 @@ export default function DropdownMenu() {
         Calculadora
       </button>
       <div
-        className={`absolute mt-2 -ml-12 w-48 bg-white border rounded-md shadow-lg z-10 transform transition-transform duration-300 ease-in-out ${
+        className={`absolute mt-2 -ml-12 w-48 bg-white dark:bg-zinc-900 border dark:border-gray-900 rounded-md shadow-lg z-10 transform transition-transform duration-300 ease-in-out ${
           isOpen ? 'scale-100 opacity-100' : 'scale-75 opacity-0 pointer-events-none ease-in'
         }`}
       >
         <a
           href="/calculadora-v1"
-          className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200 rounded-md transition duration-200"
+          className="block px-4 py-2 text-sm text-gray-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-zinc-800 rounded-md transition duration-200"
         >
           Version 1
         </a>
         <a
           href="/calculadora-v2"
-          className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200 rounded-md transition duration-200"
+          className="block px-4 py-2 text-sm text-gray-800 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-zinc-800 rounded-md transition duration-200"
         >
           Version 2
         </a>
